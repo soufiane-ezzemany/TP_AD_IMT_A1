ba1bde52a0d79e1815de7c3a89c1193d412fce3d
Soufiane EZZEMANY <soufianezzemany@gmail.com>
2023-11-12 22:32:43 +0100

    showtime grpc implementation (server)

--- showtime/showtime.py
+++ showtime/showtime.py
@@ -1,14 +1,28 @@
 import grpc
 from concurrent import futures
-from pb2 import showtime_pb2
-from pb2 import showtime_pb2_grpc
+import showtime_pb2
+import showtime_pb2_grpc
 import json
+
 
 class ShowtimeServicer(showtime_pb2_grpc.ShowtimeServicer):
 
     def __init__(self):
         with open('{}/data/times.json'.format("."), "r") as jsf:
             self.db = json.load(jsf)["schedule"]
+
+    # get the full JSON database
+    def GetSchedule(self, request, context):
+        for schedule in self.db:
+            yield showtime_pb2.ScheduleData(date=schedule['date'], movies=schedule['movies'])
+
+    # get the schedule by date
+    def GetMovieByDate(self, request, context):
+        for schedule in self.db:
+            if schedule['date'] == request.date:
+                return showtime_pb2.ScheduleData(date=schedule['date'], movies=schedule['movies'])
+        return showtime_pb2.ScheduleData(date='', movies=[])
+
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
--- showtime/showtime_pb2.py
+++ showtime/showtime_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0eshowtime.proto\"\x12\n\x04\x44\x61te\x12\n\n\x02id\x18\x01 \x01(\t\",\n\x0cScheduleData\x12\x0c\n\x04\x64\x61te\x18\x01 \x01(\t\x12\x0e\n\x06movies\x18\x02 \x03(\t\"\x07\n\x05\x45mpty2^\n\x08Showtime\x12(\n\x0bGetSchedule\x12\x06.Empty\x1a\r.ScheduleData\"\x00\x30\x01\x12(\n\x0eGetMovieByDate\x12\x05.Date\x1a\r.ScheduleData\"\x00\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0eshowtime.proto\"\x14\n\x04\x44\x61te\x12\x0c\n\x04\x64\x61te\x18\x01 \x01(\t\",\n\x0cScheduleData\x12\x0c\n\x04\x64\x61te\x18\x01 \x01(\t\x12\x0e\n\x06movies\x18\x02 \x03(\t\"\x07\n\x05\x45mpty2^\n\x08Showtime\x12(\n\x0bGetSchedule\x12\x06.Empty\x1a\r.ScheduleData\"\x00\x30\x01\x12(\n\x0eGetMovieByDate\x12\x05.Date\x1a\r.ScheduleData\"\x00\x62\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -22,11 +22,11 @@ if _descriptor._USE_C_DESCRIPTORS == False:
 
   DESCRIPTOR._options = None
   _globals['_DATE']._serialized_start=18
-  _globals['_DATE']._serialized_end=36
-  _globals['_SCHEDULEDATA']._serialized_start=38
-  _globals['_SCHEDULEDATA']._serialized_end=82
-  _globals['_EMPTY']._serialized_start=84
-  _globals['_EMPTY']._serialized_end=91
-  _globals['_SHOWTIME']._serialized_start=93
-  _globals['_SHOWTIME']._serialized_end=187
+  _globals['_DATE']._serialized_end=38
+  _globals['_SCHEDULEDATA']._serialized_start=40
+  _globals['_SCHEDULEDATA']._serialized_end=84
+  _globals['_EMPTY']._serialized_start=86
+  _globals['_EMPTY']._serialized_end=93
+  _globals['_SHOWTIME']._serialized_start=95
+  _globals['_SHOWTIME']._serialized_end=189
 # @@protoc_insertion_point(module_scope)
