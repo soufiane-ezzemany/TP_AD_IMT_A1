1ac438221a8affdf2c09be44ba70816441be3cbb
Soufiane EZZEMANY <soufianezzemany@gmail.com>
2023-11-16 12:07:58 +0100

    end TP 3
    Added the grpc booking service to user

--- user/user.py
+++ user/user.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request, jsonify, make_response
 import requests
 import json
 from werkzeug.exceptions import NotFound
+import grpc
+import booking_pb2
+import booking_pb2_grpc
 
 app = Flask(__name__)
 
@@ -27,11 +30,11 @@ def get_user_by_id(userId):
 
 @app.route("/user/<userId>/reservations", methods=['GET'])
 def get_user_reservations_by_id(userId):
-   urlBookings = f"http://{request.remote_addr}:3201/bookings/{userId}"
-   bookingsReq = requests.get(urlBookings)
 
-   if bookingsReq.status_code == 200:
-      bookings = bookingsReq.json()
+   bookingsReq = getBookingServiceData(userId)
+
+   if bookingsReq is not None:
+      bookings = bookingsReq
       for reservation in bookings["dates"]:
          reservation["movieData"] = []
          for movie in reservation["movies"]:
@@ -52,6 +55,32 @@ def get_user_reservations_by_id(userId):
       return bookings
    else:
       return make_response(jsonify({"error": "No bookings found for this user"}), 400)
+
+
+# GRPC Booking Service to get the reservations for a specific user
+def getBookingServiceData(user):
+    with grpc.insecure_channel('localhost:3002') as channel:
+        stub = booking_pb2_grpc.BookingStub(channel)
+        try:
+            bookings = stub.GetBookingForUser(booking_pb2.UserID(userid=user))
+            return json_transform(bookings)
+        except grpc.RpcError as e:
+            return None
+
+def json_transform(input_object):
+   # Initialize the result dictionary
+   result = {"userid": input_object.userid, "dates": []}
+   print(input_object.dates)
+   # Iterate over the dates
+   for date_entry in input_object.dates:
+      # Each date entry is converted to a dictionary
+      date_dict = {
+         "date": date_entry.date,
+         "movies": list(date_entry.movies)
+      }
+      result['dates'].append(date_dict)
+
+   return result
 
 
 if __name__ == "__main__":
